57d9062aa9a3ecbc4b4768a72ed16f472d19be3f
Karuna Prasanthi <karunap232@gmail.com>
2025-01-20 21:50:11 +0530

    App ready locally

--- test.py
+++
@@ -1,5 +0,0 @@
-import os
-__access_key_id = os.getenv('AWS_ACCESS_KEY_ID_ENV_KEY')
-print(__access_key_id)
-__secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY_ENV_KEY')
-print(__secret_access_key)
